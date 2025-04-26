b71b1d0ba5d5f303e3e0323c5448705181420125
fiflokonon <fiflokonon@gmail.com>
2025-04-26 13:02:40 +0100

    Self

---
+++ week5/index.py
@@ -0,0 +1,29 @@
+class Superhero:
+    def __init__(self, name, power, universe):
+        self.name = name
+        self.power = power
+        self.universe = universe
+
+    def introduce(self):
+        return f"I am {self.name} from {self.universe}, and my power is {self.power}!"
+
+    def fight(self):
+        return f"{self.name} uses {self.power} to fight evil!"
+
+class Villain(Superhero):
+    def __init__(self, name, power, universe, evil_plan):
+        super().__init__(name, power, universe)
+        self.evil_plan = evil_plan
+
+    def fight(self):
+        return f"{self.name} uses {self.power} to execute the evil plan: {self.evil_plan}!"
+
+# Example usage:
+hero = Superhero("Photon", "light manipulation", "Starverse")
+villain = Villain("Darkshade", "shadow control", "Starverse", "Eclipse the Sun")
+
+print(hero.introduce())
+print(hero.fight())
+
+print(villain.introduce())
+print(villain.fight())
